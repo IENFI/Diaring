7fa3fa835d0f2070adfa279944b17a2b6f766576
IENFI <aien0118@yu.ac.kr>
2024-03-29 18:40:49 +0900

    Initial Commit

---
+++ Diaring/DiaringApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaringApp.swift
+//  Diaring
+//
+//  Created by 하민영 on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
